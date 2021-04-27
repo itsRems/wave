70e650b32cedf3bb20bd7b4ed87b700948c140c6
Nicolas Theck <contact@itsrems.com>
2021-04-27 02:34:34 +0200

    add "Internal" model type & data.internal function

--- packages/wave/lib/collection/data.ts
+++ packages/wave/lib/collection/data.ts
@@ -6,6 +6,7 @@ export type GenericModelTypes = String | Array<any> | Object;
 export const ModelTypes = {
   PrimaryKey: 'PrimaryKey',
   Required: 'Required',
+  Internal: 'Internal',
   Secret: 'Secret',
   Index: 'Index',
   Unique: 'Unique'
@@ -16,6 +17,7 @@ export type ModelTypes = typeof ModelTypes[keyof typeof ModelTypes];
 export class Data <DataType = any> {
   public _value: DataType;
   public _secrets: string[] = [];
+  public _internals: string[] = [];
 
   constructor (public collection: () => Collection<DataType>, data: DataType) {
     this._value = data;
@@ -27,9 +29,19 @@ export class Data <DataType = any> {
   }
 
   /**
-   * Used to return a "safe" version of the data by removing any key that includes "Secret" in the model
+   * Used to return a "safe" version of the data by removing any key that includes "Secret" or "Internal" in the model
    */
   public public () {
+    const copy = { ...this._value };
+    Object.keys(copy).forEach((key) => (this._secrets.includes(key) && delete copy[key]));
+    Object.keys(copy).forEach((key) => (this._internals.includes(key) && delete copy[key]));
+    return copy;
+  }
+  
+  /**
+   * Used to return an "internal" version of the data by removing keys that include "Secret" (but not "Internal") in the model
+   */
+   public internal () {
     const copy = { ...this._value };
     Object.keys(copy).forEach((key) => (this._secrets.includes(key) && delete copy[key]));
     return copy;
@@ -51,6 +63,7 @@ export class Data <DataType = any> {
     for (const key in model) {
       const types = model[key];
       if (types.includes('Secret') && !this._secrets.includes(key)) this._secrets.push(key);
+      if (types.includes('Internal') && !this._internals.includes(key)) this._internals.push(key);      
     }
   }
 }
